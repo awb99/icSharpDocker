be45b1af34ffcadd58f576ebc130876c35224703
awb99 <andreas.wolfgang.bauer@gmail.com>
2016-03-14 13:24:45 +0100

    Update build_icsharp2.sh

--- build_icsharp2.sh
+++ build_icsharp2.sh
@@ -1,5 +1,7 @@
 # compile icsharp (the awb version)
 
+# packages directory needs to be created manually because otehrwise nuget doesnot work
+
 cd /home/condauser/icsharp \
  && . /opt/mono/env.sh \
  && mkdir /home/condauser/icsharp/packages \  

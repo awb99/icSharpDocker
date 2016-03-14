a7aeded305e4f3480fb1ee27de439581766040d8
awb99 <andreas.wolfgang.bauer@gmail.com>
2016-03-14 09:48:38 +0100

    Update build_icsharp.sh

--- build_icsharp.sh
+++ build_icsharp.sh
@@ -1,7 +1,6 @@
 # compile icsharp (the awb version)
 
-RUN git clone https://github.com/awb99/icsharp.git \
- && mono ./.nuget/NuGet.exe restore ./ScriptCs.sln \
+mono ./.nuget/NuGet.exe restore ./ScriptCs.sln \
  && xbuild ./iCSharp.sln /property:Configuration=Release /nologo /verbosity:normal
 
 #Executable is under:  icsharp/Kernel/bin/Release

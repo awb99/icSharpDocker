e89fddcdb5421b91705fd041349793f2aa72dcaa
awb99 <andreas.wolfgang.bauer@gmail.com>
2016-03-14 10:58:15 +0100

    Update build_icsharp.sh

--- build_icsharp.sh
+++ build_icsharp.sh
@@ -2,8 +2,9 @@
 
 cd icsharp
 
-mono ./.nuget/NuGet.exe restore ./ScriptCs.sln \
+. /opt/mono/env.sh \
+ && mono ./.nuget/NuGet.exe restore ./ScriptCs.sln \
  && xbuild ./iCSharp.sln /property:Configuration=Release /nologo /verbosity:normal
 
-#Executable is under:  icsharp/Kernel/bin/Release
+#Executable is under:  /home/condauser/icsharp/Kernel/bin/Release
 # mono Kernel/bin/Release/iCSharp.Kernel.exe     will show the aruments not provided; the compilation of iCSharp worked.
